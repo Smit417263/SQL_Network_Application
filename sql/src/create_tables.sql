3410d0811aebe525b096d3eda072f0f57ae06caf
Smit Patel <spate130@wch133-31.cs.ucr.edu>
2022-03-21 12:20:51 -0700

    pushing final project

---
+++ sql/src/create_tables.sql
@@ -0,0 +1,56 @@
+-- Smit Patel 862076143
+-- Gabrielle John 862104371
+DROP TABLE WORK_EXPR CASCADE;
+DROP TABLE EDUCATIONAL_DETAILS CASCADE;
+DROP TABLE MESSAGE CASCADE;
+DROP TABLE CONNECTION_USR CASCADE;
+DROP TABLE USR CASCADE;
+
+
+CREATE TABLE USR(
+	userId varchar(30) UNIQUE NOT NULL, 
+	password varchar(30) NOT NULL,
+	email text NOT NULL,
+	name char(50),
+	dateOfBirth date,
+	PRIMARY KEY(userId));
+	
+
+CREATE TABLE WORK_EXPR(
+	userId char(30) NOT NULL, 
+	company char(50) NOT NULL, 
+	role char(50) NOT NULL,
+	location char(50),
+	startDate date,
+	endDate date,
+	PRIMARY KEY(userId,company,role,startDate),
+	FOREIGN KEY(userId) REFERENCES USR(userId));
+
+CREATE TABLE EDUCATIONAL_DETAILS(
+	userId char(30) NOT NULL, 
+	instituitionName char(50) NOT NULL, 
+	major char(50) NOT NULL,
+	degree char(50) NOT NULL,
+	startdate date,
+	enddate date,
+	PRIMARY KEY(userId,major,degree),
+	FOREIGN KEY(userId) REFERENCES USR(userId));
+
+
+
+CREATE TABLE MESSAGE(
+	msgId integer UNIQUE NOT NULL, 
+	senderId char(30) NOT NULL,
+	receiverId char(30) NOT NULL,
+	contents char(500) NOT NULL,
+	sendTime timestamp,
+	deleteStatus integer,
+	status char(30) NOT NULL,
+	PRIMARY KEY(msgId));
+
+CREATE TABLE CONNECTION_USR(
+	userId char(30) NOT NULL, 
+	connectionId char(30) NOT NULL, 
+	status char(30) NOT NULL,
+	PRIMARY KEY(userId,connectionId),
+	FOREIGN KEY(userId) REFERENCES USR(userId));
